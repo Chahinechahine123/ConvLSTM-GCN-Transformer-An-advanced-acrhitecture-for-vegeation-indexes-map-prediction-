8d304af51b081250ebca47488ccff6c2848e088f
Chahine123 <131307775+Chahinechahine123@users.noreply.github.com>
2025-10-28 14:31:20 +0100

    Update Sequences creation.py

--- Data preparation/Sequences creation.py
+++ Data preparation/Sequences creation.py
@@ -4,17 +4,17 @@ import rasterio
 import os
 import matplotlib.pyplot as plt
 
-# === Dossier d'entrée (images MODIS) ===
-data_dir = "images/modis_clipped"
-seq_len = 9
+# === INPUT fiel () ===
+data_dir = "images/"
+seq_len = 12
 
-# === Chargement des fichiers .tif (float uniquement) ===
+# === load files .tif (float uniquement) ===
 files = sorted(glob.glob(os.path.join(data_dir, "*_float.tif")))
 print("✅", len(files), "images NDVI (float) trouvées.")
 if len(files) <= seq_len:
-    raise RuntimeError("❌ Pas assez d'images pour créer des séquences de test.")
+    raise RuntimeError("❌ fail to load no enough images.")
 
-# === Chargement des NDVI MODIS (normalisé [-1,1] → [0,1]) ===
+# === load NDVI  (normalisation [-1,1] → [0,1]) ===
 def read_ndvi(path):
     with rasterio.open(path) as src:
         arr = src.read(1).astype(np.float32)
@@ -25,26 +25,26 @@ def read_ndvi(path):
 stack = np.stack([read_ndvi(p) for p in files])  # (N, H, W)
 H, W = stack.shape[1:]
 
-# === Génération des séquences temporelles ===
+# === Sequence Generation  ===
 X_test, y_test = [], []
 for i in range(len(stack) - seq_len):
-    X_test.append(stack[i:i+seq_len])     # (9, H, W)
+    X_test.append(stack[i:i+seq_len])     # (12, H, W)
     y_test.append(stack[i+seq_len])       # (H, W)
 
-X_test = np.array(X_test)[..., np.newaxis]   # (N, 9, H, W, 1)
+X_test = np.array(X_test)[..., np.newaxis]   # (N, 12, H, W, 1)
 y_test = np.array(y_test)[..., np.newaxis]   # (N, H, W, 1)
 
-print("✅ Données MODIS préparées pour test :")
+
 print("X_test:", X_test.shape, "y_test:", y_test.shape)
 
-# === Sauvegarde ===
-out_dir = "brute_data/modis"
+
+out_dir = ""
 os.makedirs(out_dir, exist_ok=True)
 
 np.save(os.path.join(out_dir, "X_test.npy"), X_test)
 np.save(os.path.join(out_dir, "y_test.npy"), y_test)
 
-# === Aperçu d’un exemple ===
+# === show some exemples ===
 plt.figure(figsize=(8, 3.5))
 plt.subplot(1, 2, 1)
 plt.imshow(X_test[0, -1, :, :, 0], cmap="RdYlGn")
