c7de8a2e89e604668974913c4809c89426e6eacc
Chahinechahine123 <bouazizchahine7@gmail.com>
2025-10-28 16:07:14 +0100

    tte

--- Train and validation/train.py
+++ Train and validation/train.py
@@ -13,16 +13,16 @@ class RealTimeRAMMonitor(Callback):
     def on_train_batch_end(self, batch, logs=None):
         ram = psutil.virtual_memory()
         loss = logs.get('loss')
-        print(f"[Batch {batch+1}] Loss: {loss:.4f} | RAM utilisée : {ram.used / (1024**3):.2f} Go / {ram.total / (1024**3):.2f} Go ({ram.percent}%)")
+        print(f"[Batch {batch+1}] Loss: {loss:.4f} | RAM used : {ram.used / (1024**3):.2f} Go / {ram.total / (1024**3):.2f} Go ({ram.percent}%)")
 
 
 
-X_train = np.load("brute_data/X_train.npy")
-y_train = np.load("brute_data/y_train.npy")
-X_test = np.load("brute_data/X_test.npy")
-y_test = np.load("brute_data/y_test.npy")
+X_train = np.load("data/X_train.npy")
+y_train = np.load("data/y_train.npy")
+X_test = np.load("X_test.npy")
+y_test = np.load("y_test.npy")
 
-model = build_model_convlstm(input_shape=X_train.shape[1:])
+model = build_model_convlstm_gcn_transformer(input_shape=X_train.shape[1:])
 checkpoint = ModelCheckpoint("data/model.keras", save_best_only=True, monitor="val_loss", mode="min")
 csv_logger = CSVLogger("train/training11_log.csv", append=True)
 ReduceLR=ReduceLROnPlateau(patience=5, factor=0.5, verbose=1)
